c9918cba4e5bd8988c96a8f1d58d1af1126f76d0
MinghongGao <gmh601066513@gmail.com>
2017-08-22 00:24:36 +1000

    Detailed Comment On Diamond Square

--- Assets/scripts_oliver/DiamondSquare_c.cs
+++ Assets/scripts_oliver/DiamondSquare_c.cs
@@ -4,48 +4,70 @@ using UnityEngine;
 
 public class DiamondSquare_c : MonoBehaviour {
 
-	public int mDivisions;
+	public int divisionsCount;
 	public float mSize;
 	public float mHeight;
 
-	Vector3[] mVerts;
-	int mVertCount;
+	Vector3[] vertices;
+	int totalvertices;
 
 	// Use this for initialization
 	void Start () {
 		CreateTerrain ();
 	}
-	
 
+    // Create height map
 	void CreateTerrain () {
-		mVertCount = (mDivisions + 1) * (mDivisions + 1);
-		mVerts = new Vector3[mVertCount];
-		Vector2[] uvs = new Vector2[mVertCount];
-		int[] tris = new int[mDivisions * mDivisions * 6];
+        // All vertices in height map
+		totalvertices = (divisionsCount + 1) * (divisionsCount + 1);
+        // Store vertices in one dimentional array
+		vertices = new Vector3[totalvertices];
+		Vector2[] uvs = new Vector2[totalvertices];
+        // The number of triangles : disvisionsCount * discisionsCount * 2
+        // Each triangle in Unity have three vertices to store
+        // Thus, the factor is 6.
+		int[] tris = new int[divisionsCount * divisionsCount * 6];
 
+        //half size of the terrain
 		float halfSize = mSize * 0.5f;
-		float divisionSize = mSize / mDivisions;
+        //each small square size
+		float divisionSize = mSize / divisionsCount;
 
+        // Create a new mesh
 		Mesh mesh = new Mesh ();
-
 		GetComponent<MeshFilter> ().mesh = mesh;
 
+        // To count triangle vertices,
+        // 6 count per square,
+        // 3 count per triangle,
+        // increment 6 count per loop to calculate the next group
 		int triOffset = 0;
 
-		for(int i = 1; i <=mDivisions; i++) {
-			for(int j = 0; j<=mDivisions;j++) {
-				mVerts[i * (mDivisions+1)+j] = new Vector3(-halfSize+j*divisionSize,0.0f,halfSize-i*divisionSize);
-				uvs[i*(mDivisions+1)+j] = new Vector2((float)i/mDivisions,(float)j/mDivisions);
+		for(int i = 1; i <=divisionsCount; i++) {
+			for(int j = 0; j<=divisionsCount;j++) {
+                // Set vertices
+				vertices[i * (divisionsCount+1)+j] = new Vector3(-halfSize+j*divisionSize,0.0f,halfSize-i*divisionSize);
+                // Set uv inorder to applu texture
+				uvs[i*(divisionsCount+1)+j] = new Vector2((float)i/divisionsCount,(float)j/divisionsCount);
 
-				if(i < mDivisions && j < mDivisions) {
-					int topLeft = i *(mDivisions+1)+j;
-					int botLeft = (i+1)*(mDivisions+1)+j;
+                //build triangles
+				if(i < divisionsCount && j < divisionsCount) {
+					int topLeft = i *(divisionsCount+1)+j;
+					int botLeft = (i+1)*(divisionsCount+1)+j;
 
-
+                    //\---
+                    // \ |
+                    //  \|
+                    //   \
 					tris[triOffset] =topLeft;
 					tris[triOffset+1] = topLeft+1;
 					tris[triOffset+2] = botLeft+1;
 
+                    //|
+                    //|\
+                    //| \
+                    //|  \
+                    //|---\
 					tris[triOffset+3] = topLeft;
 					tris[triOffset+4] = botLeft+1;
 					tris[triOffset+5] = botLeft;
@@ -55,22 +77,35 @@ public class DiamondSquare_c : MonoBehaviour {
 				}
 			}
 		}
+        // End of setting up flat surface
+
 
 		//Algorithm
+        //Start from four edges
 
-		mVerts [0].y = Random.Range (-mHeight, mHeight);
-		mVerts [mDivisions].y = Random.Range (-mHeight, mHeight);
-		mVerts [mVerts.Length - 1].y = Random.Range (-mHeight, mHeight);
-		mVerts [mVerts.Length - 1 - mDivisions].y = Random.Range (-mHeight, mHeight);
+        //top left
+		vertices [0].y = Random.Range (-mHeight, mHeight);
+        //top right
+		vertices [divisionsCount].y = Random.Range (-mHeight, mHeight);
+        //bottom right
+		vertices [vertices.Length - 1].y = Random.Range (-mHeight, mHeight);
+        //bottom left
+		vertices [vertices.Length - 1 - divisionsCount].y = Random.Range (-mHeight, mHeight);
 
-		int iterations = (int)Mathf.Log (mDivisions, 2);
+
+        // number of iterations needed to be performed based on the division
+		int iterations = (int)Mathf.Log (divisionsCount, 2);
+
+        //initial number of square
 		int numSquares = 1;
-		int squareSize = mDivisions;
+
+		int squareSize = divisionsCount;
 		for(int i = 0;i < iterations; i++) {
 
 			int row =0;
 			for(int j = 0; j < numSquares; j++) {
 				int col = 0;
+                // iterater each square
 				for (int k = 0; k < numSquares; k++) {
 					DiamondSquare (row, col, squareSize, mHeight);
 					col += squareSize;
@@ -79,33 +114,37 @@ public class DiamondSquare_c : MonoBehaviour {
 			}
 			numSquares *= 2;
 			squareSize /= 2;
-			mHeight *= 0.5f;//Modify
+            //Modify to control the change of height
+			mHeight *= 0.5f;
 		}
 
 
 
 		// apply to the height map
-		mesh.vertices =mVerts;
+		mesh.vertices =vertices;
 		mesh.uv = uvs;
 		mesh.triangles=tris;
 
 		mesh.RecalculateBounds();
 		mesh.RecalculateNormals();
-		 
+
 	}
 
 	void DiamondSquare(int row, int col, int size, float offset) {
 		int halfSize = (int)(size*0.5f);
-		int topLeft = row * (mDivisions+1)+col;
-		int botLeft = (row+size) * (mDivisions+1) + col;
-
-		int mid = (int)(row+halfSize) * (mDivisions+1) + (int)(col+halfSize);
-		mVerts[mid].y = (mVerts[topLeft].y+mVerts[topLeft+size].y+mVerts[botLeft].y+mVerts[botLeft+size].y)*0.25f+ Random.Range(-offset,offset);
-
-		mVerts [topLeft + halfSize].y = (mVerts [topLeft].y + mVerts [topLeft + size].y + mVerts [mid].y) / 3 + Random.Range (-offset, offset);
-		mVerts[mid-halfSize].y = (mVerts[topLeft].y+mVerts[botLeft].y+mVerts[mid].y)/3 + Random.Range (-offset, offset);
-		mVerts[mid+halfSize].y = (mVerts[topLeft+size].y+mVerts[botLeft+size].y+mVerts[mid].y)/3 + Random.Range (-offset, offset);
-		mVerts[botLeft+halfSize].y = (mVerts[botLeft].y+mVerts[botLeft+size].y+mVerts[mid].y)/3 + Random.Range (-offset, offset);
+        //top left index of the square
+		int topLeft = row * (divisionsCount+1)+col;
+        //bottom left index of the square
+		int botLeft = (row+size) * (divisionsCount+1) + col;
+        // middle point index of the square
+		int mid = (int)(row+halfSize) * (divisionsCount+1) + (int)(col+halfSize);
+        //calculate the mid point value based on four points+ random value--Diamond Step
+		vertices[mid].y = (vertices[topLeft].y+vertices[topLeft+size].y+vertices[botLeft].y+vertices[botLeft+size].y)*0.25f+ Random.Range(-offset,offset);
+        //calculate the mid point value based on three points+ random value--Square Step
+		vertices [topLeft + halfSize].y = (vertices [topLeft].y + vertices [topLeft + size].y + vertices [mid].y) / 3 + Random.Range (-offset, offset);
+		vertices[mid-halfSize].y = (vertices[topLeft].y+vertices[botLeft].y+vertices[mid].y)/3 + Random.Range (-offset, offset);
+		vertices[mid+halfSize].y = (vertices[topLeft+size].y+vertices[botLeft+size].y+vertices[mid].y)/3 + Random.Range (-offset, offset);
+		vertices[botLeft+halfSize].y = (vertices[botLeft].y+vertices[botLeft+size].y+vertices[mid].y)/3 + Random.Range (-offset, offset);
 
 	}
 }
